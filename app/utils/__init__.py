1082d0dc9d58ddca585b4613f98d90441ab69e67
Python Full-stack <57242242+pythonfullstack@users.noreply.github.com>
2021-12-18 09:25:29 -0800

    update

--- app/utils/__init__.py
+++ app/utils/__init__.py
@@ -1,4 +1,3 @@
 from .upload import upload_data_with_zip, upload_data_with_streaming, storage
 
 client = storage.Client.from_service_account_json('/secrets/cloudsql/prod.json')
-# client = storage.Client.from_service_account_info(info=prod_json)
